dbe8ed4b2d565306874c86cdb53374d2d4e64949
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2023-06-12 23:13:27 +0600

    update classes by instructor

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yaanftr.mongodb.net/?retryWrites=true&w=majority`
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -119,6 +119,8 @@ async function run() {
             const result = await classes.toArray()
             res.send(result)
         })
+
+        //update classes by user
 
         //post data to classes from instructor json in db
         app.post("/classes", async (req, res) => {
